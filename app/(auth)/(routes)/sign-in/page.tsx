6a266226860fe3297f1730c83ece586920897486
Cipto Hadi <ciptohadi79@gmail.com>
2023-10-17 10:45:15 +0700

    add credit link to Hanko in sign-in page

--- app/(auth)/(routes)/sign-in/page.tsx
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -51,6 +51,16 @@ export default function Login() {
   return (
     <div className="flex min-h-screen justify-center items-center bg-gray-900">
       <div className="bg-gray-50 p-5 rounded-2xl shadow-md">
+        <div className="text-gray-900">
+          Auth by{" "}
+          <a
+            href="https://hanko.io/"
+            target="_blank"
+            className="font-semibold hover:underline text-red-700"
+          >
+            Hanko
+          </a>
+        </div>
         <hanko-auth />
       </div>
     </div>
